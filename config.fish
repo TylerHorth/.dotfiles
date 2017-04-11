4c63adbf211ae4d9d509a04cf55f0828b5d9e32f
Tyler Horth <tylerhorth@outlook.com>
2017-04-11 18:55:20 -0400

    ssh support

--- config.fish
+++ config.fish
@@ -1,4 +1,7 @@
 fish_vi_key_bindings
+
+set fish_term24bit 1
+
 set -l GRUVBOX_SCRIPT ~/.vim/plugged/gruvbox/gruvbox_256palette.sh
 if test -f $GRUVBOX_SCRIPT
   bash $GRUVBOX_SCRIPT
@@ -27,22 +30,41 @@ function fish_mode_prompt --description 'Displays the current mode'
     switch $fish_bind_mode
       case default
         set_color --bold --background a89984 282828
-        echo ' NORMAL '
-        set_color --bold --background 504945 a89984
+        echo ' N '
+        if test -n $SSH_CLIENT
+          set_color --bold --background a89984 504945
+        else
+          set_color --bold --background 504945 a89984
+        end
+        echo ''
       case insert
         set_color --bold --background 83a598 282828
-        echo ' INSERT '
-        set_color --bold --background 504945 83a598
+        echo ' I '
+        if test -n $SSH_CLIENT
+          set_color --bold --background a89984 83a598
+        else
+          set_color --bold --background 504945 83a598
+        end
+        echo ''
       case replace-one
         set_color --bold --background 8ec07c 282828
-        echo ' REPLACE'
-        set_color --bold --background 504945 8ec07c
+        echo ' R '
+        if test -n $SSH_CLIENT
+          set_color --bold --background a89984 8ec07c
+        else
+          set_color --bold --background 504945 8ec07c
+        end
+        echo ''
       case visual
         set_color --bold --background d79921 282828
-        echo ' VISUAL '
-        set_color --bold --background 504945 d79921
+        echo ' V '
+        if test -n $SSH_CLIENT
+          set_color --bold --background a89984 d79921
+        else
+          set_color --bold --background 504945 d79921
+        end
+        echo ''
     end
-    echo ''
     set_color normal
   end
 end
